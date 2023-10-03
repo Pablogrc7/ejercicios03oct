52395cf8f60e575b543159bf5ff90ab7727cad72
unknown <Pablo>
2023-10-03 14:40:04 -0700

    trabajo

---
+++ script.js
@@ -0,0 +1,24 @@
+//Ejercicio 1
+//let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//let results = arr.filter((num) => num >= 5);
+//results.forEach(alert)
+
+
+//Ejercicio 2
+//let arr = [" Naranja ", " Platano ", " Manzana ", " Fresa ", " Kiwi "];
+//alert(arr.slice(2, 4));
+
+//Ejercicio 3
+//let arr = [5, 10, 15, 20];
+//arr.map((funSuma, i) => console.log(i+":"+ (funSuma+10)));
+
+//Ejercicio 4
+//let arr = ["H", "I", "J"];
+//let arr2 = ["K", "L", "M"];
+//let arr3= arr.concat(arr2);
+//console.log(arr3)
+
+//Ejercicio 5
+//let arr = ["Mexico", "USA", "Francia"];
+//alert(arr.includes("Francia"));
+
